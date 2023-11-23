62acefd654967bbbecd3a3b1729f00e32f6620f7
LeitnerMi <michael.leitner@gepardec.com>
2023-11-23 16:29:07 +0100

    working send and receive example

--- argocd/install-argocd-operator.sh
+++ argocd/install-argocd-operator.sh
@@ -12,11 +12,11 @@ printf "\nArgoCD UI is starting. Your username is $username and password is $pas
 
 kubectl apply -f argocd/yaml-templates/argocd-ingress.yaml
 
-kubectl apply -f argocd/yaml-templates/cert-manager-operator.yaml -n argocd
-# kubectl apply -f argocd/yaml-templates/rabbbitmq-topology-operator.yaml -n argocd
-kubectl apply -f argocd/yaml-templates/rabbitmq-cluster-operator-project.yaml -n argocd
+# kubectl apply -f argocd/yaml-templates/cert-manager-operator.yaml -n argocd
+# kubectl apply -f argocd/yaml-templates/rabbitmq-cluster-operator-project.yaml -n argocd
 # kubectl apply -f https://github.com/rabbitmq/messaging-topology-operator/releases/latest/download/messaging-topology-operator-with-certmanager.yaml -n rabbitmq-system
 
+# kubectl apply -f argocd/yaml-templates/rabbbitmq-topology-operator.yaml -n argocd
 
 # cmd="kubectl port-forward svc/argocd-server -n argocd 8080:443"
 # $cmd > /dev/null &
---
+++ install-all.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+printf "Starting to create cluster, then installing argocd, then installing rabbitmq... \n\n"
