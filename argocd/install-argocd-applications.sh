cbc6ab450e1de0604da5f2af910cb8fb5dd9b63b
LeitnerMi <michael.leitner@gepardec.com>
2023-11-24 07:41:13 +0100

    added user and secrets

--- argocd/install-argocd-applications.sh
+++ argocd/install-argocd-applications.sh
@@ -2,5 +2,12 @@
 
 kubectl apply -f argocd/yaml-templates/basic-rabbitmq-cluster-application.yaml -n argocd
 
+sleep 10s
+
 kubectl apply -f argocd/yaml-templates/poc-app-application.yaml -n argocd
 
+while ! kubectl get secret hello-world-default-user -n hello-world-cluster; do echo "Waiting for RabbitMQ-cluster secret. CTRL-C to exit."; sleep 1; done
+
+username="$(kubectl get secret hello-world-default-user -n hello-world-cluster -o jsonpath='{.data.username}' | base64 --decode)"
+password="$(kubectl get secret hello-world-default-user -n hello-world-cluster -o jsonpath='{.data.password}' | base64 --decode)"
+printf "You can start RabbitMQ Management-UI with |kubectl rabbitmq -n hello-world-cluster manage hello-world|. Your username is $username and password is $password \n\n"
