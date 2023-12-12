dbd769e25d99cba701687b0633d99971adec6dc6
LeitnerMi <michael.leitner@gepardec.com>
2023-12-12 10:49:41 +0100

    added sealed secrets

---
+++ sealed-secrets/install-sealed-secrets.sh
@@ -0,0 +1,8 @@
+kubectl apply -f https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.24.4/controller.yaml
+
+kubectl apply -f sealed-secrets-cert-secret.yaml -n kube-system
+kubectl delete pod -l name=sealed-secrets-controller -n kube-system
+
+
+
+
